d952abb5a4160924c4a62a60b0134aa657c89fda
Daniella Fegi <114934334+Yuf0ria@users.noreply.github.com>
2025-10-22 21:24:56 +0800

    ;

---
+++ Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    //Checks
+    int i ;
+
+    //gameobejct
+    public GameObject Scube, MCube,LCube;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checking
+        // if(Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 1.5, Random.Range(-10, 11));
+        //     Instantiate(cubePrefab, randomSpawnPosition, Quaternion.identity);
+        //     //Ok done sa check
+        // }
+
+    }
+
+    void SpawnCubes()
+    {
+        
+    }
+}
